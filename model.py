a7037cd7baf8697a3c992078fa4713dc343c2fa7
jafbCromabox <joseflores.cromabox@gmail.com>
2021-02-06 09:19:39 -0600

    MODELO DE CADENA ACTUALIZADO

--- model.py
+++ model.py
@@ -5,8 +5,10 @@ class Cadena():
     def __init__(self):
         self.id = 0
         self.nombre = ""
-        self.ordenar = False
-        self.buscar = False
+        self.esOrdenado = False
+        self.ordenado = []
+        self.esBuscado = False
+        self.buscado = {}
         self.numeros = []
 
 # SETTERS
@@ -17,11 +19,17 @@ class Cadena():
     def setNombre(self, nombre):
         self.nombre = nombre
 
-    def setOrdenar(self, ordenar):
-        self.ordenar = ordenar
+    def setEsOrdenado(self, esOrdenado):
+        self.esOrdenado = esOrdenado
 
-    def setBuscar(self, buscar):
-        self.buscar = buscar
+    def setOrdenado(self, ordenado):
+        self.ordenado = ordenado
+
+    def setEsBuscado(self, esBuscado):
+        self.esBuscado = esBuscado
+
+    def setBuscado(self, buscado):
+        self.buscado = buscado
 
     def setNumeros(self, numeros):
         self.numeros = numeros
@@ -37,11 +45,17 @@ class Cadena():
     def getNombre(self):
         return self.nombre
 
-    def getOrdenar(self):
-        return self.ordenar
+    def getEsOrdenado(self):
+        return self.esOrdenado
 
-    def getBuscar(self):
-        return self.buscar
+    def getOrdenado(self):
+        return self.ordenado
+
+    def getEsBuscado(self):
+        return self.esBuscado
+
+    def getBuscado(self):
+        return self.buscado
     
     def getNumeros(self):
         return self.numeros
