4c6585dd98465a6266d3fa9775325b00926dff88
jafbCromabox <joseflores.cromabox@gmail.com>
2021-02-08 16:19:46 -0600

    CARGA DE ARCHIVO 1

---
+++ lectorArchivos.py
@@ -0,0 +1,10 @@
+import easygui
+
+#Lector de archivox
+def leerArchivo():
+    archivo = easygui.fileopenbox( default="./data/*.lfp")
+    data = []
+    with open(archivo, "r", encoding="utf-8") as f: 
+        for line in f.readlines():
+            data.append(line.replace('\n', ''))
+    return data
